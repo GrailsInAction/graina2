d69b4f0ddc4f600330a27b82a2906678958c3a1f
Peter Ledbrook <peter@cacoethes.co.uk>
2009-02-01 16:00:53 +0000

    Added unit test for PostRestController.

--- hubbub/grails-app/controllers/PostRestController.groovy
+++ hubbub/grails-app/controllers/PostRestController.groovy
@@ -1,12 +1,29 @@
+import grails.converters.JSON
+
 class PostRestController {
     def list = {
-        [ posts: Post.list() ]
+        def postList = Post.list()
+        withFormat {
+            xml {
+                [ posts: postList ]
+            }
+            json {
+                render postList as JSON
+            }
+        }
     }
 
     def show = {
         def post = Post.get(params.id)
         if (post) {
-            return [ post: post ]
+            withFormat {
+                xml {
+                    [ post: post ]
+                }
+                json {
+                    render post as JSON
+                }
+            }
         }
         else {
             response.sendError(404)
@@ -17,21 +34,38 @@ class PostRestController {
         Post post = new Post(params["post"])
 
         if (post.save(flush: true)) {
-            render contentType: "application/xml", {
-                // Return the ID of the new post.
-                id(post.id)
+            withFormat {
+                xml {
+                    render contentType: "application/xml", {
+                        // Return the ID of the new post.
+                        id(post.id)
+                    }
+                }
+                json {
+                    render( [ success: true, id: post.id ] as JSON )
+                }
             }
         }
         else {
-            response.status = 403
-            render contentType: "application/xml", {
-                errors {
-                    post.errors.fieldErrors.each { err ->
-                        validation {
-                            field(err.field)
-                            message(g.message(error: err))
+            response.status = 405
+            withFormat {
+                xml {
+                    render contentType: "application/xml", {
+                        errors {
+                            post.errors.fieldErrors.each { err ->
+                                validation {
+                                    field(err.field)
+                                    message(g.message(error: err))
+                                }
+                            }
                         }
                     }
+                }
+                json {
+                    def errorList = post.errors.fieldErrors.collect { err ->
+                        [ field: err.field, message: g.message(error: err) ]
+                    }
+                    render errorList as JSON
                 }
             }
         }
