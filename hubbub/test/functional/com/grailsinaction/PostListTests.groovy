a60674ad5856083f001b04d21a4245e539f2a1d0
Peter Ledbrook <peter@cacoethes.co.uk>
2009-02-13 18:25:17 +0000

    Added the Functional Test plugin.

---
+++ hubbub/test/functional/com/grailsinaction/PostListTests.groovy
@@ -0,0 +1,33 @@
+package com.grailsinaction
+
+class PostListTests extends functionaltestplugin.FunctionalTestCase {
+    void testTimelineNotLoggedIn() {
+        get("/post/list")
+        assertStatus 500
+    }
+
+    void testTimeline() {
+        // User must log in first.
+        post("/login/index") {
+            userId = "peter"
+            password = "password"
+        }
+
+        // Check that the timeline page was loaded.
+        assertRedirectUrlContains "/post/list"
+
+        // Fetch the page.
+        get("/post/list")
+        assertTitle "Hubbub » New Post"
+        assertContentContains "What are you hacking on right now?"
+
+        // Post a new message.
+        def testMsg = "Functional test message"
+        form("postMessage") {
+            postContent = testMsg
+            click "Post"
+        }
+
+        assertContentContains "peter " + testMsg
+    }
+}
