1ed2cbb715f83cf4b7441538ec7a523dab5ef0c6
Peter Ledbrook <peter@cacoethes.co.uk>
2009-04-02 07:29:58 +0100

    Added timeline page.

--- hubbub/grails-app/controllers/com/grailsinaction/PostController.groovy
+++ hubbub/grails-app/controllers/com/grailsinaction/PostController.groovy
@@ -1,7 +1,77 @@
 package com.grailsinaction
 
+import grails.converters.*
+
 class PostController {
 
+    def postService
+
     def scaffold = true
+
+    def index = {
+        if (!params.id)
+            params.id = "chuck_norris"
+        redirect(action: 'timeline', params: params)
+    }
+
+    def timeline = {
+        def user = User.findByUserId(params.id)
+        [ user : user ]
+    }
+
+    def addPost = {
+        try {
+            def newPost = postService.createPost(params.id, params.content)
+            flash.message = "Added new post: ${newPost.content}"
+        } catch (PostException pe) {
+            flash.message = pe.message
+        }
+        redirect(action: 'timeline', id: params.id)
+    }
+
+
+    def recentPosts = {
+
+        def user = User.findByUserId(params.id)
+        def posts = Post.findAllByUser(user, [max: 5])
+
+        withFormat {
+            js { 
+                render(contentType:"text/json") {
+                    hubbubPosts(user: user.userId) {
+                        posts.each { p ->
+                            post(contents: p.content,
+                                created:p.dateCreated)
+                        }
+                    }
+				
+                }
+            }
+            xml { render posts.encodeAsXML() }
+        }
+
+
+    }
+
+   
+
+    /* Refactor old addPost() action into the PostService */
+    /*
+    def addPost = {
+        def user = User.findByUserId(params.id)
+        if (user) {
+            def post = new Post(params)
+            if (post.validate()) {
+                user.addToPosts(post)
+                flash.message = "Successfully created Post"
+            } else {
+                flash.message = "Invalid or empty post"
+            }
+        } else {
+            flash.message = "Invalid User Id"
+        }
+        redirect(action: 'timeline', id: params.id)
+    }
+    */
 
 }
