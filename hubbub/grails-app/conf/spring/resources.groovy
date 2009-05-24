1452408c15b93aa60fc1c8dd05bc0b8fc7d05398
Peter Ledbrook <peter@cacoethes.co.uk>
2009-05-24 22:57:03 +0100

    Enabled Hibernate statistics collection via resources.groovy.

--- hubbub/grails-app/conf/spring/resources.groovy
+++ hubbub/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,7 @@
+import org.springframework.jmx.support.MBeanServerFactoryBean
+import org.springframework.jmx.export.MBeanExporter
+import org.hibernate.jmx.StatisticsService
+
 // Place your Spring DSL code here
 beans = {
     // This is required for the HTTP Basic authentication to work properly.
@@ -5,5 +9,20 @@ beans = {
     // for an unauthenticated user, rather than the default 302.
     authenticationEntryPoint(org.springframework.security.ui.basicauth.BasicProcessingFilterEntryPoint) {
         realmName = 'Hubbub'
+    }
+
+    // Hibernate statistics collection.
+    hibernateStats(StatisticsService) {
+        statisticsEnabled = true
+        sessionFactory = ref("sessionFactory")
+    }
+
+    mbeanServer(MBeanServerFactoryBean) {
+        locateExistingServerIfPossible = true
+    }
+
+    exporter(MBeanExporter) {
+        server = mbeanServer
+        beans = ["org.hibernate:name=statistics": hibernateStats]
     }
 }
