be9dac5d898af908126acbdbd4967bd08023bf3d
Glen Smith <glen@bytecode.com.au>
2012-05-31 16:03:33 +1000

    Upgraded project to Grails 2.0 and refreshed qotd sample to be Grails 2.0 happy

--- source-code/ch01/qotd/grails-app/conf/Config.groovy
+++ source-code/ch01/qotd/grails-app/conf/Config.groovy
@@ -6,13 +6,18 @@
 //                             "file:${userHome}/.grails/${appName}-config.properties",
 //                             "file:${userHome}/.grails/${appName}-config.groovy"]
 
-// if(System.properties["${appName}.config.location"]) {
+// if (System.properties["${appName}.config.location"]) {
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
+
+
+grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
+// grails.project.groupId = "com.grailsinaction.qotd" // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
+grails.mime.use.accept.header = false
 grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
                       xml: ['text/xml', 'application/xml'],
-                      text: 'text-plain',
+                      text: 'text/plain',
                       js: 'text/javascript',
                       rss: 'application/rss+xml',
                       atom: 'application/atom+xml',
@@ -23,48 +28,67 @@ grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
                       form: 'application/x-www-form-urlencoded',
                       multipartForm: 'multipart/form-data'
                     ]
-// The default codec used to encode data with ${}
-grails.views.default.codec="none" // none, html, base64
-grails.views.gsp.encoding="UTF-8"
-grails.converters.encoding="UTF-8"
 
+// URL Mapping Cache Max Size, defaults to 5000
+//grails.urlmapping.cache.maxsize = 1000
+
+// What URL patterns should be processed by the resources plugin
+grails.resources.adhoc.patterns = ['/images/*', '/css/*', '/js/*', '/plugins/*']
+
+
+// The default codec used to encode data with ${}
+grails.views.default.codec = "none" // none, html, base64
+grails.views.gsp.encoding = "UTF-8"
+grails.converters.encoding = "UTF-8"
+// enable Sitemesh preprocessing of GSP pages
+grails.views.gsp.sitemesh.preprocess = true
+// scaffolding templates configuration
+grails.scaffolding.templates.domainSuffix = 'Instance'
+
+// Set to false to use the new Grails 1.2 JSONBuilder in the render method
+grails.json.legacy.builder = false
 // enabled native2ascii conversion of i18n properties files
 grails.enable.native2ascii = true
+// packages to include in Spring bean scanning
+grails.spring.bean.packages = []
+// whether to disable processing of multi part requests
+grails.web.disable.multipart=false
+
+// request parameters to mask when logging exceptions
+grails.exceptionresolver.params.exclude = ['password']
+
+// enable query caching by default
+grails.hibernate.cache.queries = true
 
 // set per-environment serverURL stem for creating absolute links
 environments {
+    development {
+        grails.logging.jul.usebridge = true
+    }
     production {
-        grails.serverURL = "http://www.changeme.com"
+        grails.logging.jul.usebridge = false
+        // TODO: grails.serverURL = "http://www.changeme.com"
     }
 }
 
 // log4j configuration
-log4j {
-    appender.stdout = "org.apache.log4j.ConsoleAppender"
-    appender.'stdout.layout'="org.apache.log4j.PatternLayout"
-    appender.'stdout.layout.ConversionPattern'='[%r] %c{2} %m%n'
-    appender.stacktraceLog = "org.apache.log4j.FileAppender"
-    appender.'stacktraceLog.layout'="org.apache.log4j.PatternLayout"
-    appender.'stacktraceLog.layout.ConversionPattern'='[%r] %c{2} %m%n'
-    appender.'stacktraceLog.File'="stacktrace.log"
-    rootLogger="error,stdout"
-    logger {
-        grails="error"
-        StackTrace="error,stacktraceLog"
-        org {
-            codehaus.groovy.grails.web.servlet="error"  //  controllers
-            codehaus.groovy.grails.web.pages="error" //  GSP
-            codehaus.groovy.grails.web.sitemesh="error" //  layouts
-            codehaus.groovy.grails."web.mapping.filter"="error" // URL mapping
-            codehaus.groovy.grails."web.mapping"="error" // URL mapping
-            codehaus.groovy.grails.commons="info" // core / classloading
-            codehaus.groovy.grails.plugins="error" // plugins
-            codehaus.groovy.grails.orm.hibernate="error" // hibernate integration
-            springframework="off"
-            hibernate="off"
-        }
-    }
-    additivity.StackTrace=false
+log4j = {
+    // Example of changing the log pattern for the default console
+    // appender:
+    //
+    //appenders {
+    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+    //}
+
+    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
+           'org.codehaus.groovy.grails.web.pages', //  GSP
+           'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+           'org.codehaus.groovy.grails.web.mapping', // URL mapping
+           'org.codehaus.groovy.grails.commons', // core / classloading
+           'org.codehaus.groovy.grails.plugins', // plugins
+           'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+           'org.springframework',
+           'org.hibernate',
+           'net.sf.ehcache.hibernate'
 }
-
-
--- source-code/ch01/qotd/grails-app/views/quote/random.gsp
+++ source-code/ch01/qotd/grails-app/views/quote/random.gsp
@@ -1,29 +1,25 @@
 <html>
 <head>
-	<title>Random Quote</title>
-	<g:javascript library="prototype" />
+    <title>Random Quote</title>
+    <g:javascript library="jquery" />
 </head>
-
 <body>
-	
-	<ul id="menu">
-		<li>
-		<g:remoteLink action="ajaxRandom" update="quote">
-			Next Quote
-		</g:remoteLink>
-		</li>
-		<li>
-		<g:link action="list">
-			Admin
-		</g:link>
-		</li>		
-	</ul>
-	
-	<div id="quote">
-		<q>${quote.content}</q>
-		<p>${quote.author}</p>
-	</div>
-		
-</body>
+    <ul id="menu">
+        <li>
+            <g:remoteLink action="ajaxRandom" update="quote">
+                Next Quote
+            </g:remoteLink>
+        </li>
+        <li>
+            <g:link action="list">
+                Admin
+            </g:link>
+        </li>
+    </ul>
 
+    <div id="quote">
+        <q>${quote.content}</q>
+        <p>${quote.author}</p>
+    </div>
+</body>
 </html>
