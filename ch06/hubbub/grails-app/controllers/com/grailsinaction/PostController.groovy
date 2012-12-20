4f1534fe3bcd88053bd3ae7e6754dba462785381
Peter Ledbrook <peter@cacoethes.co.uk>
2012-12-20 12:20:46 +0000

    Add timeline and post submission to PostController (ch06).

--- ch06/hubbub/grails-app/controllers/com/grailsinaction/PostController.groovy
+++ ch06/hubbub/grails-app/controllers/com/grailsinaction/PostController.groovy
@@ -2,4 +2,27 @@ package com.grailsinaction
 
 class PostController {
     static scaffold = true
+
+    def timeline() {
+        def user = User.findByLoginId(params.id)
+        [ user : user ]
+    }
+
+    def addPost() {
+        def user = User.findByLoginId(params.id)
+        if (user) {
+            def post = new Post(params)
+            user.addToPosts(post)
+            if (user.save()) {
+                flash.message = "Successfully created Post"
+            } else {
+                user.discard()
+                flash.message = "Invalid or empty post"
+            }
+        } else {
+            flash.message = "Invalid User Id"
+        }
+        redirect(action: 'timeline', id: params.id)
+    }
+    
 }
