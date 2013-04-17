87495857926179497f3b585df19961fee5582d70
Peter Ledbrook <peter@cacoethes.co.uk>
2013-04-17 09:40:04 +0100

    Use version 1.2 of Resources plugin.

--- ch03/hubbub/grails-app/conf/BuildConfig.groovy
+++ ch03/hubbub/grails-app/conf/BuildConfig.groovy
@@ -28,7 +28,7 @@ grails.project.dependency.resolution = {
     plugins {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.7.2"
-        runtime ":resources:1.1.6"
+        runtime ":resources:1.2"
 
         build ":tomcat:$grailsVersion"
 
