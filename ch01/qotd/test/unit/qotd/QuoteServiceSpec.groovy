8ba71960b7245f70f165de499667702659f28deb
Glen Smith <glen@bytecode.com.au>
2014-02-03 17:09:58 +1100

    Ported old Junit test to Spock

--- ch01/qotd/test/unit/qotd/QuoteServiceSpec.groovy
+++ ch01/qotd/test/unit/qotd/QuoteServiceSpec.groovy
@@ -3,18 +3,17 @@ package qotd
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
- */
 @TestFor(QuoteService)
 class QuoteServiceSpec extends Specification {
 
-    def setup() {
-    }
+    void "static quote service always returns quiche quote"() {
 
-    def cleanup() {
-    }
+            when:
+            Quote staticQuote = service.getStaticQuote()
 
-    void "test something"() {
+            then:
+            staticQuote.author == "Anonymous"
+            staticQuote.content == "Real Programmers Don't eat quiche"
+
     }
 }
