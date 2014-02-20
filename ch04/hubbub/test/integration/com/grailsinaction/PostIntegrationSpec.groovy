74e5e8e38380552e9b08c16aaa2388404a38dbad
Peter Ledbrook <peter@cacoethes.co.uk>
2014-02-20 15:31:07 +0000

    Sync ch04 source with ch03 source.

--- ch04/hubbub/test/integration/com/grailsinaction/PostIntegrationSpec.groovy
+++ ch04/hubbub/test/integration/com/grailsinaction/PostIntegrationSpec.groovy
@@ -1,18 +1,17 @@
 package com.grailsinaction
 
 import spock.lang.*
-import grails.plugin.spock.*
 
-class PostIntegrationSpec extends IntegrationSpec  {
+class PostIntegrationSpec extends Specification {
 
     def "Adding posts to user links post to user"() {
 
         given: "A brand new user"
         def user = new User(loginId: 'joe',                   
-                            password: 'secret').save(failOnError: true)                   
+                            password: 'secret').save(failOnError: true)
 
         when: "Several posts are added to the user"
-        user.addToPosts(new Post(content: "First post... W00t!"))               
+        user.addToPosts(new Post(content: "First post... W00t!"))
         user.addToPosts(new Post(content: "Second post..."))
         user.addToPosts(new Post(content: "Third post..."))
 
@@ -20,21 +19,20 @@ class PostIntegrationSpec extends IntegrationSpec  {
         3 == User.get(user.id).posts.size()
     }
 
-
     def "Ensure posts linked to a user can be retrieved"() {
 
         given: "A user with several posts"
-        def user = new User(loginId: 'joe', password: 'secret').save(failOnError: true)
-        user.addToPosts(new Post(content: "First"))                     
-        user.addToPosts(new Post(content: "Second"))                   
-        user.addToPosts(new Post(content: "Third"))                     
+        User user = new User(loginId: 'joe', password: 'secret').save(failOnError: true)
+        user.addToPosts(new Post(content: "First"))
+        user.addToPosts(new Post(content: "Second"))
+        user.addToPosts(new Post(content: "Third"))
 
         when: "The user is retrieved by their id"
-        def foundUser = User.get(user.id) 
+        def foundUser = User.get(user.id)
         List<String> sortedPostContent = foundUser.posts.collect { it.content }.sort()
 
         then: "The posts appear on the retrieved user"
-        sortedPostContent == ['First', 'Second', 'Third']          
+        sortedPostContent == ['First', 'Second', 'Third']
         
     }
 
@@ -42,20 +40,20 @@ class PostIntegrationSpec extends IntegrationSpec  {
 
         given: "A user with a set of tags"
         def user = new User(loginId: 'joe', password: 'secret').save(failOnError: true)
-        def tagGroovy = new Tag(name: 'groovy')             
-        def tagGrails = new Tag(name: 'grails')             
-        user.addToTags(tagGroovy)                     
+        def tagGroovy = new Tag(name: 'groovy')
+        def tagGrails = new Tag(name: 'grails')
+        user.addToTags(tagGroovy)
         user.addToTags(tagGrails)
 
         when: "The user tags two fresh posts"
-        def groovyPost = new Post(content: "A groovy post")          
-        user.addToPosts(groovyPost)                 
-        groovyPost.addToTags(tagGroovy)             
+        def groovyPost = new Post(content: "A groovy post")
+        user.addToPosts(groovyPost)
+        groovyPost.addToTags(tagGroovy)
         
-        def bothPost = new Post(content: "A groovy and grails post")  
-        user.addToPosts(bothPost)          
+        def bothPost = new Post(content: "A groovy and grails post")
+        user.addToPosts(bothPost)
         bothPost.addToTags(tagGroovy)
-        bothPost.addToTags(tagGrails)                  
+        bothPost.addToTags(tagGrails)
 
         then:
         user.tags*.name.sort() == [ 'grails', 'groovy']
@@ -63,6 +61,6 @@ class PostIntegrationSpec extends IntegrationSpec  {
         2 == bothPost.tags.size()
 
     }
-
+    
 
 }
