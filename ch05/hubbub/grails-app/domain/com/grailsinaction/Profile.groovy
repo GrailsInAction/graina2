b0525932753d21d48d4aac8d19bc9875c516adeb
Peter Ledbrook <peter@cacoethes.co.uk>
2014-03-29 13:02:18 +0000

    Fix size for Profile's photo property.

--- ch05/hubbub/grails-app/domain/com/grailsinaction/Profile.groovy
+++ ch05/hubbub/grails-app/domain/com/grailsinaction/Profile.groovy
@@ -16,7 +16,7 @@ class Profile {
         bio nullable: true, maxSize: 1000
         homepage url: true, nullable: true
         email email: true, blank: false
-        photo nullable: true
+        photo nullable: true, maxSize: 2 * 1024 * 1024 
         country nullable: true
         timezone nullable: true
         jabberAddress email: true, nullable: true
