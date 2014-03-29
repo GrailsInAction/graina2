d72b0464290052a61671636f08ae05f1b4eb2e09
Peter Ledbrook <peter@cacoethes.co.uk>
2014-03-29 09:24:47 +0000

    Reformat integration tests in ch03 source.

--- ch03/hubbub/test/integration/com/grailsinaction/UserIntegrationSpec.groovy
+++ ch03/hubbub/test/integration/com/grailsinaction/UserIntegrationSpec.groovy
@@ -22,7 +22,8 @@ class UserIntegrationSpec extends Specification {
     def "Updating a saved user changes its properties"() {
 
         given: "An existing user"
-        def existingUser = new User(loginId: 'joe', password: 'secret').save(failOnError: true)
+        def existingUser = new User(loginId: 'joe', password: 'secret')
+        existingUser.save(failOnError: true)
 
         when: "A property is changed"
         def foundUser = User.get(existingUser.id)    
@@ -37,7 +38,8 @@ class UserIntegrationSpec extends Specification {
     def "Deleting an existing user removes it from the database"() {
 
         given: "An existing user"
-        def user = new User(loginId: 'joe', password: 'secret').save(failOnError: true)
+        def user = new User(loginId: 'joe', password: 'secret')
+        user.save(failOnError: true)
 
         when: "The user is deleted"
         def foundUser = User.get(user.id)
@@ -51,7 +53,7 @@ class UserIntegrationSpec extends Specification {
     def "Saving a user with invalid properties causes an error"() {
 
         given: "A user which fails several field validations"
-        def user = new User(loginId: 'chuck_norris', password: 'tiny')
+        def user = new User(loginId: 'joe', password: 'tiny')
 
         when:  "The user is validated"
         user.validate()
@@ -70,7 +72,7 @@ class UserIntegrationSpec extends Specification {
     def "Recovering from a failed save by fixing invalid properties"() {
 
         given: "A user that has invalid properties"
-        def chuck = new User(loginId: 'chuck_norris', password: 'tiny')
+        def chuck = new User(loginId: 'chuck', password: 'tiny')
         assert chuck.save()  == null
         assert chuck.hasErrors()
 
