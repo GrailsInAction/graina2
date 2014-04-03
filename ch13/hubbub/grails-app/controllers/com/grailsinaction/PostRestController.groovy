3df581154cef3a0d9e91bd9257ffa254651ad772
Peter Ledbrook <peter@cacoethes.co.uk>
2014-04-03 08:38:36 +0100

    Fix RestFilters for DELETEs from browser.

--- ch13/hubbub/grails-app/controllers/com/grailsinaction/PostRestController.groovy
+++ ch13/hubbub/grails-app/controllers/com/grailsinaction/PostRestController.groovy
@@ -24,8 +24,6 @@ class PostRestController {
         }
     }
 
-
-
     def save(PostDetails post) {
         if (!post.hasErrors()) {
             def user = springSecurityService.currentUser
@@ -39,64 +37,38 @@ class PostRestController {
         }
     }
 
-    def update(Long id) {
-        if (id == null) {
-            response.sendError 405
-            return
-        }
+    def update(Long id, PostDetails postDetails) {
+        if (!postDetails.hasErrors()) {
+            def post = Post.get(id)
 
-        def post = Post.get(id)
-        if (post) {
-            //bindData post, params.post
-            post.content = params.message
+            if (!post) {
+                respond new ErrorDetails(error: "Not found"), status: 404
+                return
+            }
 
-            def body
-            if (post.validate() && post.save()) {
-                response.status = 200
-                body = [id: post.id]
-                //render status: 200, [id: post.id] as XML
-            }
-            else {
-                response.status = 400
-                body = [error: "Invalid data"]
-                //render status: 400, [error: "Invalid data"] as XML
-            }
-            withFormat {
-                json { render body as JSON }
-                xml { render body as XML }
-            }
+            post.content = postDetails.message
+            post.validate() && post.save()
+            respond post
         }
         else {
-            response.sendError 404
+            respond postDetails
         }
     }
 
     def delete(Long id) {
         def body
-
-        if (id == null) {
-            response.status = 405
-            body = [message: "Cannot DELETE without an ID"]
-        }
-
+        def status
         if (Post.exists(id)) {
             Post.load(id).delete()
-            body = [message: "Post with ID $id deleted"]
+            status = 200
+            body = new ErrorDetails(error: "Post with ID $id deleted")
         }
         else {
-            response.status = 404
-            body = [message: "Not found"]
+            status = 404
+            body = new ErrorDetails(error: "Not found")
         }
 
-        withFormat {
-            json {
-                render body as JSON
-            }
-
-            xml {
-                render body as XML
-            }
-        }
+        respond body, status: status
     }
     
 }
@@ -107,4 +79,8 @@ class PostDetails {
     static constraints = {
         message blank: false, nullable: false
     }
+}
+
+class ErrorDetails {
+    String error
 }
