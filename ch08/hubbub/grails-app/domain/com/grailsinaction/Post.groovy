bcd4664409ef08bc51afaba99e110419d65b6194
Peter Ledbrook <peter@cacoethes.co.uk>
2014-06-23 10:10:15 +0100

    Fix scaffolding bug with Post.

--- ch08/hubbub/grails-app/domain/com/grailsinaction/Post.groovy
+++ ch08/hubbub/grails-app/domain/com/grailsinaction/Post.groovy
@@ -15,12 +15,12 @@ class Post {
         sort dateCreated: "desc"
     }
     
-    String toString() { return "Post '${shortContent}' (id: $id) for user '${user.loginId}'" }
+    String toString() { return "Post '${shortContent}' (id: $id) for user '${user?.loginId}'" }
     String getDisplayString() { return shortContent }
 
     String getShortContent() {
         def maxSize = 20
-        if (content.size() > maxSize) return content.substring(0, maxSize - 3) + '...'
+        if (content?.size() > maxSize) return content.substring(0, maxSize - 3) + '...'
         else return content
     }
 
